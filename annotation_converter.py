25b567af7f6189aec8f9cce3e16b07dcb6f01538
Elena <dilase@usi.ch>
2018-02-22 15:52:12 +0100

    start

---
+++ annotation_converter.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from datetime import timedelta
+import datetime
+import time
+from os.path import basename
+import os
+
+# Convert the annotation table got from ANVIL into a DataFrame
+def annotation_conversion(filename):
+  df = pd.read_csv(filename,delimiter="\t")
+  return df
+
+# Look whether in a folder there is a video and if it is, return the filename without extension,
+# This corresponds to the beginning of the video expressed as: ddmmyyhhmmss
+def video_name_converter(dirpath):
+ for subdir, dirs, files in os.walk(dir):
+     for file in files:
+        if (file.endswith(".mp4")):
+            os.path.splitext(file)
+            filename = os.path.splitext(file)[0]
+ return filename
+
+
+# Convert the filename expressed as: ddmmyyhhmmss into a time stamp in the format: yy-mm-dd hh:mm:ss
+def filename_converter(dir_path):
+ file_name = video_name_converter(dir_path)
+ day = file_name[0:2]
+ month = file_name[2:4]
+ year = file_name[4:8]
+ hour = file_name[8:10]
+ minutes = file_name[10:12]
+ seconds = file_name[12:14]
+ experiment_day = datetime.datetime(int(year),int(month),int(day), int(hour), int(minutes),int(seconds))
+
+ return experiment_day
+
+# Create an annotaion db with additional columns: start_converted and end_converted.
+# start_converted: experiment_day + seconds when the laughter started in yy-mm-dd hh:mm:ss format
+# end_converted: experiment_day + seconds when the laughter ended in yy-mm-dd hh:mm:ss
+def annotation_time_converter(directory, filepath):
+    db = annotation_conversion(filepath)
+    day_ex = filename_converter(directory)
+    start = db['start'].values
+    end = db['end'].values
+    start_converted = []
+    end_converted = []
+    for i in range(0, len(start)):
+        start_converted.append((day_ex + timedelta(seconds=start[i])).strftime("%Y-%m-%d %H:%M:%S"))
+        end_converted.append((day_ex + timedelta(seconds=end[i])).strftime("%Y-%m-%d %H:%M:%S"))
+
+    db['start_converted'] = start_converted
+    db['end_converted'] = end_converted
+
+    db.to_csv(filepath, sep="\t", index=0)
+
+    return db
+
+
+
+
+if __name__ == '__main__':
+    dir = "C:\Users\user\Desktop\Pilot_Study/"
+    file = 'additional_annotation.txt'
+    user = 'u001/'
+
+    annotation_time_converter(dir,dir + user + file)
